db34cb751c967430d9e00c23e35ae11f4ece7fcc
ZebdaYacine <zebdaadam1996@gmail.com>
2025-03-25 05:19:55 +0100

    update gitigone

--- frontend/cnr/vite.config.ts
+++ frontend/cnr/vite.config.ts
@@ -1,13 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import fs from "fs";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   server: {
     https: {
-      key: fs.readFileSync("./src/cert/cnr-key.pem"),
-      cert: fs.readFileSync("./src/cert/cnr.pem"),
+      key: fs.readFileSync(
+        path.resolve(__dirname, "../../backend/cert/cnr-key.pem")
+      ),
+      cert: fs.readFileSync(
+        path.resolve(__dirname, "../../backend/cert/cnr.pem")
+      ),
     },
     host: "localhost",
     port: 5173,
